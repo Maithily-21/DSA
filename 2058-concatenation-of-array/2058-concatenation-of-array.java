d682ab42634d0474a1ea843407d9d14ea7a728cf
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2025-10-16 23:23:34 +0530

    Time: 1 ms (96.85%), Space: 45.5 MB (32.1%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -6,12 +6,7 @@ class Solution {
 
         for(int i = 0 ; i < n ; i++){
                 ans[i] = nums[i];
-        }
-
-        int j = 0 ;
-        for(int i = n ; i < 2*n  ; i++){
-                ans[i] = nums[j];
-                j++;
+                ans[i+n] = nums[i];
         }
 
         return ans;
