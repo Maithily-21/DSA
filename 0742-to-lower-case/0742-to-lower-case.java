9f6f361feb520b8ccdeaedb2dc95d35e678a27bc
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2025-10-22 23:43:17 +0530

    Time: 0 ms (100%), Space: 41.6 MB (78.72%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,16 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-
-            if(ch >= 'A' || ch <= 'Z'){
-                ch = Character.toLowerCase(ch);
-            }
-            sb.append(ch);
-        }
-
-        return sb.toString();
+        return s.toLowerCase();
     }
 }
