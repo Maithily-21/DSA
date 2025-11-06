ba728ebb2ac055a968cf4f0846a60fbebc79b1e0
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2025-11-06 05:38:51 +0530

    Time: 0 ms (100%), Space: 41.5 MB (5.08%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,15 +1,29 @@
 class Solution {
     public int numberOfSteps(int num) {
-            int count = 0 ;
-          while(num > 0 ){
-            if(num % 2 == 0 ){
-                 num = num / 2;
-            }else{
-                num = num - 1 ;
-            }
-            count++;
-          }
 
-       return count;
+        // Brute forvce 
+    //         int count = 0 ;
+    //       while(num > 0 ){
+    //         if(num % 2 == 0 ){
+    //              num = num / 2;
+    //         }else{
+    //             num = num - 1 ;
+    //         }
+    //         count++;
+    //       }
+
+    //    return count;
+
+
+    // using recursion 
+        if(num == 0){
+            return 0;
+        }
+        if(num % 2 == 0 ){
+               return 1 + numberOfSteps(num/2);
+        }else{
+              return 1 + numberOfSteps(num - 1);
+        }
+
     }
 }
