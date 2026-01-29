eab851d49accdc908db97d368dc52aef171f7afb
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2026-01-29 23:25:23 +0530

    Time: 1151 ms (40.13%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,9 +1,13 @@
 /* Write your PL/SQL query statement below */
 
 
-SELECT v.customer_id, COUNT(*) AS count_no_trans
-FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id
-WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id;
+/* Write your PL/SQL query statement below */
+Select v.customer_id,
+    COUNT(v.visit_id) as count_no_trans
+    from visits v
+    left join
+    transactions t
+    on v.visit_id = t.visit_id
+    where t.visit_id is NULL
+    group by v.customer_id
+    having COUNT(v.visit_id) > 0;
