78996b38d2ccad7514a9e4e91e63cd2536202197
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2026-02-04 22:22:25 +0530

    Time: 1357 ms (5.21%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,8 +1,6 @@
 /* Write your PL/SQL query statement below */
-
-select user_id , count(follower_id) as followers_count 
-from Followers 
-group by user_id 
-order by user_id ;
+select user_id,count(*) as followers_count from Followers
+group by user_id
+order by user_id;
 
 
