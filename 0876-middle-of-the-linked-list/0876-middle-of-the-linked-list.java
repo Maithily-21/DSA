d4a5f7ae334f5b6f29a00d0092be0246faadf547
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2026-06-07 12:30:06 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.75%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,29 +10,47 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+
+        //Brute Force ==>O(N+N/2)
+        // if(head == null || head.next == null){
+        //     return head;
+        // }
+        // int count = 0 ;
+        // ListNode temp = head;
+        // while(temp != null ){
+        //     count++;
+        //     temp = temp.next;
+        // }
+        
+        // int mid = count/2 + 1;
+        // temp = head ;
+        // while(temp!=null){
+        //     mid--;
+        //     if(mid == 0){
+        //         break;
+        //     }
+        //     temp=temp.next;
+        // }
+        // return temp;
+        
+        //==============================================================
+        //OPTIMAL APPROACH - Tortoise & Hare Approach 
+        //Slow and fast Pointer 
+
         if(head == null || head.next == null){
             return head;
         }
-        int count = 0 ;
-        ListNode temp = head;
-        while(temp != null ){
-            count++;
-            temp = temp.next;
-        }
-        
-        int mid = count/2 + 1;
-        temp = head ;
+        ListNode slow = head ;
+        ListNode fast = head;
 
-        while(temp!=null){
-            mid--;
-            if(mid == 0){
-                break;
-            }
-            temp=temp.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next; //one nade at a time
+            fast = fast.next.next; // two nodes at a time 
         }
 
-        return temp;
+        //when fast reaches at end ....slow pointer will be at middle of LL
 
+        return slow;
        
     }
 }
