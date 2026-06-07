ca37a84fb1c8446cb006313a4ddb0c3652218820
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2026-06-07 14:38:47 +0530

    Time: 0 ms (100%), Space: 46.6 MB (85.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -16,17 +16,31 @@ public class Solution {
         // TC : O(NlogN), SC = O(N)
         //We will use HashMap to store the data and index of nodes visited , 
         //if we found a visited node again it means loop is present 
-        ListNode temp = head ;
-        //track of  nodes 
-        HashMap<ListNode,Integer> map = new HashMap<>();
+        // ListNode temp = head ;
+        // //track of  nodes 
+        // HashMap<ListNode,Integer> map = new HashMap<>();
      
-        while(temp != null){
-            //if node is already present , loop is present 
-            if(map.containsKey(temp)){
+        // while(temp != null){
+        //     //if node is already present , loop is present 
+        //     if(map.containsKey(temp)){
+        //         return true;
+        //     }
+        //     map.put(temp , 1);
+        //     temp = temp.next;
+        // }/
+
+        // Optimised Approach : Slow and fast pointer , tc - O(N) , SC - O(1)
+        ListNode slow = head ;
+        ListNode fast = head ;
+        if(head == null || head.next == null){
+            return false;
+        }
+        while(fast!=null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
                 return true;
             }
-            map.put(temp , 1);
-            temp = temp.next;
         }
 
         return false;
