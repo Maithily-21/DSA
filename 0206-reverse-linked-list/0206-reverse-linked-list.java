5a1c30728ebbecae4453c1e352e7677925c9131f
Maithily Patle <155337582+Maithily-21@users.noreply.github.com>
2026-06-07 14:04:57 +0530

    Time: 0 ms (100%), Space: 44.5 MB (13.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,24 +11,39 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
 
-        //Brute Force -TIME COMPLEXITY :  O(2N) , SPACE COMPLEXITY : O(N)
-        Stack<Integer> stack = new Stack<>();
-        ListNode temp = head ; 
+        //Brute Force -TIME COMPLEXITY :O(2N) , SPACE COMPLEXITY : O(N)
+        // Stack<Integer> stack = new Stack<>();
+        // ListNode temp = head ; 
 
-        //1] Traversing the LL and storing the values in Stack 
-        while(temp != null){
-            stack.push(temp.val);
-            temp=temp.next;
+        // //1] Traversing the LL and storing the values in Stack 
+        // while(temp != null){
+        //     stack.push(temp.val);
+        //     temp=temp.next;
+        // }
+
+        // temp =  head ;
+        // //2] poping out the stack values and replacing the data values with top values of stack 
+        // while(temp != null){
+        //     temp.val = stack.peek();
+        //     stack.pop();
+        //     temp=temp.next;
+        // }
+
+        // return head;
+        if(head == null || head.next==null){
+            return head;
         }
 
-        temp =  head ;
-        //2] poping out the stack values and replacing the data values with top values of stack 
-        while(temp != null){
-            temp.val = stack.peek();
-            stack.pop();
-            temp=temp.next;
+        //OPTIMAL Approach : TIME COMPLEXITY :O(N) , SPACE COMPLEXITY : O(1)
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode front ;
+        while(temp!= null){
+            front = temp.next; 
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
-
-        return head;
+        return prev;
     }
 }
